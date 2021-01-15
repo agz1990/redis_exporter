e60b11f8395c59443ca6ea78e50e19a92c56979b
agz1990 <522360568@qq.com>
2021-01-15 10:01:54 +0800

    Update and rename pwd_file.go to 222pwd_file.go

--- exporter/222pwd_file.go
+++ exporter/222pwd_file.go
@@ -1,11 +1,11 @@
-package exporter
+ epackage exporter
 
 import (
 	"encoding/json"
 	"io/ioutil"
 
 	log "github.com/sirupsen/logrus"
-)
+)s's's'z
 
 // LoadPwdFile reads the redis password file and returns the password map
 func LoadPwdFile(passwordFile string) (map[string]string, error) {
